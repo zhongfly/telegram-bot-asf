dc354daff6d96879b0ec71d42cb5150e90e714c1
zhongfly <11155705+zhongfly@users.noreply.github.com>
2022-05-13 09:58:34 +0800

    对特殊字符进行转义
    markdownV2要求转义特殊字符

--- telegram-asf.py
+++ telegram-asf.py
@@ -1,5 +1,6 @@
 # encoding:UTF-8
 # python3.6
+from telegram.utils.helpers import escape_markdown
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, ConversationHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from functools import wraps
@@ -268,7 +269,7 @@ def deal_command(context, chat_id):
         if pattern_2fa_command.match(command):
             res = re.sub(r'([A-Z0-9]{5}$)',r'`\1`',res)
             bot.editMessageText(
-                chat_id=chat_id, message_id=chat_data['msg'], text=res, parse_mode='MarkdownV2')     
+                chat_id=chat_id, message_id=chat_data['msg'], text=escape_markdown(res, version=2), parse_mode='MarkdownV2')     
             context.job_queue.run_once(mfa_timeout, 30, context=(
                 chat_id, chat_data['msg']))
         else:
@@ -368,7 +369,7 @@ def reply(update, context):
     res = send(command)
     if pattern_2fa_command.match(command):
         res = re.sub(r'([A-Z0-9]{5}$)',r'`\1`',res)
-        msg = update.message.reply_text(text=res, quote=True, parse_mode='MarkdownV2')
+        msg = update.message.reply_text(text=escape_markdown(res, version=2), quote=True, parse_mode='MarkdownV2')
         context.job_queue.run_once(mfa_timeout, 30, context=(chat_id, msg.message_id))
     else:
         msg = update.message.reply_text(text=res, quote=True)
